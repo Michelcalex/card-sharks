2e1db7e15b15c1459494676794a2cc2b0d9fded9
Lexi <Michelcalex+github@gmail.com>
2016-12-28 13:11:50 -0500

    Took back of cards function out. Cleaned and organized code. GET request works and shows one drawn card. Buttons higher and lower are displayed as well.

--- js/card-sharks.js
+++ js/card-sharks.js
@@ -1,57 +1,37 @@
-console.log('I am working!');
-
 // STEPS:
-// 1-Display a card when the page first loads 
-// 1a- use deck of cards API to fetch new cards
+// 1-Display a card when the page first loads  ✓
+// 1a- use deck of cards API to fetch new cards ✓
 // 2-There should be a higher or lower button displayed along the cards
 // 3-When the player clicks a button, the new card should be chosen and displayed along with the player's guess
+// 3a- if the player guesses correctly, move on to the next card and set the value to that value's card
+// 3b- if the player is wrong, alert game over
 
 
 window.addEventListener('load', function() {
-    getCards();
-    gameButtons();
+   drawCard();
+
 });
 
 
-function getCards() {
+function drawCard() {
     let request = new XMLHttpRequest();
     request.open('GET', 'https://deckofcardsapi.com/api/deck/new/draw/?count=1');
-    request.addEventListener('load', function() {
-        console.log('we got cards!');
-
-        let cardContainer = document.querySelector('#show-playing-cards');
-
+    request.addEventListener('load', function () {
+        console.log('The first GET is working');
         let response = JSON.parse(request.responseText);
-        for(let i = 0; i < response.cards.length; i++) {
-            let imageHolder = document.createElement('img');
-            imageHolder.src = response.cards[i].image;
-            cardContainer.appendChild(imageHolder);
-        }
-
-        for(let i = 0; i < 4; i++) {
-            if (i < 4) {
-                let backOfCard = document.createElement('div');
-                backOfCard.classList.add('backCard');
-                cardContainer.appendChild(backOfCard);
-            }
-        }
+        console.log('Response is through');
+        showCards(response.cards[0]);
     });
 
     request.send();
+    
 }
 
 
+function showCards(card) {
+    let cardContainer = document.querySelector('#show-playing-cards');
 
-function gameButtons() {
-    let higherBtn = document.querySelector('#higher');
-    higherBtn.addEventListener('click', function() {
-        console.log('I think its higher');
-    });
-
-    let lowerBtn = document.querySelector('#lower');
-    lowerBtn.addEventListener('click', function() {
-        console.log('I think its lower');
-    });
+    let cardImageHolder = document.createElement('img');
+    cardImageHolder.src = card.image;
+    cardContainer.appendChild(cardImageHolder);
 }
-
-
